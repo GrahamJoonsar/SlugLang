e60ccd5f480c5ba025d70be6766dfb2a1f661334
Graham Joonsar <70826772+GrahamJoonsar@users.noreply.github.com>
2021-06-07 17:49:09 -0400

    Put the interpretation processes in a class, and added variables.
    Added a integer, float, and string variable types. and they can be printed out using the print function

--- slug.cpp
+++ slug.cpp
@@ -2,67 +2,11 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include "slugInterpreter.h"
 
-struct Function{
-    std::string name; // Name of the function
-    const int argc; // the amount of arguments it takes
-    void (*actualFunc)(std::string*); // the actual function
-};
-
-std::vector<Function> functions;
-
-int argcountForFunc = 1;
-int funcNum;
-
-bool inFunctions(std::string potentialFunc){
-    int i = 0;
-    for (Function fun : functions){
-        if (potentialFunc == fun.name){
-            argcountForFunc = fun.argc;
-            funcNum = i;
-            return true;
-        }
-        i++;
-    }
-    return false;
-}
-
-std::vector<std::string> tokenizer(std::string passedInString){
-    std::vector<std::string> tokens;
-    std::string token;
-    bool isString = false;
-    for (int i = 0; i < passedInString.length(); i++){ // looping through string
-        if (passedInString[i] != ' ' && !isString){
-            if (passedInString[i] == '"'){ // string started
-                isString = true;
-            } else {
-                token += passedInString[i];
-            }
-        } else if (isString){
-            if (passedInString[i] != '"'){
-                token += passedInString[i];
-            } else {
-                isString = false;
-            }
-        } else {
-            tokens.push_back(token);
-            token = "";
-        }
-    }
-    tokens.push_back(token); // adding last token
-    return tokens;
-}
-
-void print(std::string * printString){
-    std::cout << printString[0] << std::endl;
-}
+Interpreter slugInterp;
 
 int main(int argc, char * argv[]){
-    // Functions definition
-    
-    functions.push_back({"print", 1, print}); // print function
-
-
     // Reading the command line arguments
     if (argc == 2){
         std::cout << "Starting interpretation" << std::endl;
@@ -89,17 +33,16 @@ int main(int argc, char * argv[]){
     std::string currentLine;
     std::vector<std::string> currentLineTokens;
     std::string args[10]; // Hopefully not more that 10 arguments
-    bool functionCalled = false;
 
     while (getline(inputFile, currentLine)){
-        currentLineTokens = tokenizer(currentLine);
+        currentLineTokens = slugInterp.tokenizer(currentLine);
         for (int i = 0; i < currentLineTokens.size(); i++){ // looping through tokens
-            if (inFunctions(currentLineTokens[i])){ // function was called, and getting the number of arguments for the funvtion
-                for (int j = 0; j < argcountForFunc; j++){
+            if (slugInterp.inFunctions(currentLineTokens[i])){ // function was called
+                for (int j = 0; j < slugInterp.argcountForFunc; j++){
                     i++;
                     args[j] = currentLineTokens[i];
                 }
-                functions[funcNum].actualFunc(args);
+                slugInterp.functions[slugInterp.funcNum].actualFunc(args, &slugInterp);
             }
         }
     }
