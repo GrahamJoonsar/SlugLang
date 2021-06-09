b1f1767ab02af860d6f59d794c7e7ae685aa5d17
Graham Joonsar <70826772+GrahamJoonsar@users.noreply.github.com>
2021-06-09 14:14:09 -0400

    Added new vars to support if statements

--- Source Code/slugInterpreter.h
+++ Source Code/slugInterpreter.h
@@ -21,16 +21,23 @@ class Interpreter{
     public:
         int argcountForFunc = 1;
         int funcNum;
+        bool curlyBraceLevel[10][2]; // Hopefully not more than 10 layer of curly braces
+        int curlyBraceNum = 0;
+        bool isValid = true;
         std::vector<Function> functions; // standard functions for the interpreter
+
+        /* User declared Variables */
         std::map<std::string, int> integers; // integers that are declared
         std::map<std::string, float> floats; // floats that are declared
         std::map<std::string, std::string> strings; // stringss that are declared
+        std::map<std::string, bool> booleans; // booleans that are declared
 
         Interpreter();
 
         bool inFunctions(std::string potentialFunc);
-
         bool inInts(std::string potentialInt);
+        bool inFloats(std::string potentialInt);
+        bool inStrings(std::string potentialInt);
 
         std::vector<std::string> tokenizer(std::string passedInString);
 };
