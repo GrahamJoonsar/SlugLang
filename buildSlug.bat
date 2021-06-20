5dcc7abeb1562e1817c029b3b78c5e1fdd1213bc
GrahamJoonsar <70826772+GrahamJoonsar@users.noreply.github.com>
2021-06-19 20:09:48 -0400

    Changed build file to work with the repository and changed exe to release

--- buildSlug.bat
+++ buildSlug.bat
@@ -1,9 +1,8 @@
 @echo off
 echo Starting Build
 :: Building for Debug, Not including libraries
-if %1 == debug g++ src/slug.cpp src/slugInterpreter.cpp -o build/slug.exe
+if %1 == debug g++ src/slug.cpp src/slugInterpreter.cpp -o slug.exe
 :: Building for release, using libraries
 :: for windows, dunno about other os
-if %1 == release g++ -static-libgcc -static-libstdc++ src/slug.cpp src/slugInterpreter.cpp -o build/slug.exe
+if %1 == release g++ -static-libgcc -static-libstdc++ src/slug.cpp src/slugInterpreter.cpp -o slug.exe
 echo Build Complete
-:: Paths might be different if you cloned the repository
